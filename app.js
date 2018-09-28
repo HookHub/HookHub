e0ea71b503c1e9ad5ca6bc6f0fb121fa1a4215eb
Ty Eggen <tycho.eggen@gmail.com>
2018-09-28 13:56:36 -0700

    Initial version

---
+++ app.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var debug = require('debug')('hookhub:app');
+
+var index = require('./routes/index');
+
+var hooks = require('express-middleware-module-loader')('./hooks/');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+app.use('/hooks', hooks);
+
+app.get('/dump', function (req, res, next) {
+  debug("Registered routes:");
+  debug("========================================");
+  app._router.stack.forEach(function (r) {
+      debug("r:", r);
+  });
+  debug("========================================");
+  res.send({result:"OK"});
+});
+
+// catch 404 and forward to error handler
+debug("Adding 404");
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+debug("Adding error handler");
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
